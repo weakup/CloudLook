83bb1b65280192637a114074882027b98f9cc9e3
weakup <916149034@qq.com>
2018-02-26 17:42:24 +0800

    个人侧栏选项

--- app/src/main/java/com/example/lisiyan/cloudlook/adapter/WelfareAdapter.java
+++ app/src/main/java/com/example/lisiyan/cloudlook/adapter/WelfareAdapter.java
@@ -31,6 +31,13 @@ public class WelfareAdapter extends BaseRecyclerViewAdapter<GankIoDataBean.Resul
         @Override
         public void onBindViewHolder(final GankIoDataBean.ResultBean resultsBean, final int posotion) {
 
+            /**
+             * 注意：DensityUtil.setViewMargin(itemView,true,5,3,5,0);
+             * 如果这样使用，则每个item的左右边距是不一样的，
+             * 这样item不能复用，所以下拉刷新成功后显示会闪一下
+             * 换成每个item设置上下左右边距是一样的话，系统就会复用，就消除了图片不能复用 闪跳的情况
+             */
+
             if (posotion % 2 ==0){
                 DensityUtil.setViewMargin(itemView,false,12,6,12,0);
             }else {
